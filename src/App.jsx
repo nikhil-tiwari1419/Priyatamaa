f982bb4e8ab2b41b02072526a1c6726b7f08d2bc
nikhil-tiwari1419 <nikhiltiwari1425@gmail.com>
2025-10-19 21:11:22 +0530

    ::

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     
-      <BrowserRouter basename="/Priyatamaa">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Firstpage />}/>
           <Route path="/SecondPage" element={<SecondPage />}/>
