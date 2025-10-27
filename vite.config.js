9d08f6ec2bab37fc8cd899995ff45028e929c4d7
nikhil-tiwari1419 <nikhiltiwari1425@gmail.com>
2025-10-27 17:56:36 +0530

    Adding file and code to resolve refresh 404 error

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   historyApiFallback: true,
-  // },
   base: "/",
-})
+  server: {
+    historyApiFallback: true,
+  },
+});
