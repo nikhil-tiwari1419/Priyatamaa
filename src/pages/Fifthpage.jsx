51ebbc38760e36c40dc548373a0c277ebaacf389
nikhil-tiwari1419 <nikhiltiwari1425@gmail.com>
2025-10-27 17:47:14 +0530

    update 5th page

--- src/pages/Fifthpage.jsx
+++ src/pages/Fifthpage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Fifthpage() {
   return (
-    <div className='bg-white min-h-screen mx-auto max-w-md'> 
+    <div className='bg-white/80 pt-34 px-10 min-h-screen mx-auto text-black max-w-md'> 
     kuch nahi hai yaha!   if U have idea DM Karo 🫰
          <img src="https://media.tenor.com/1Qykugf_fgsAAAAi/question-mark-question.gif" alt="" />
         <div className="text-center text-black bottom-0 mt-12 text-sm">
